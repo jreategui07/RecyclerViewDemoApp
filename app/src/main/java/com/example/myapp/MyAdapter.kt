02b3d7244f366c29700aeeb673deaaecc74f44ba
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-07-21 22:57:47 -0400

    Updating comments and removing unused class

--- app/src/main/java/com/example/myapp/MyAdapter.kt
+++ app/src/main/java/com/example/myapp/MyAdapter.kt
@@ -6,11 +6,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapp.databinding.RowLayoutBinding
 
-
-// 1. MyAdapter constructor parameter: What is the list of data you want to display?
+// 1. MyAdapter constructor parameter: Setting the list of data to display and interface
 class MyAdapter(val myItems:MutableList<String>, val clickInterface:ClickDetectorInterface) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
-    // 2. ViewHolder constructor parameter: What is the layout you should use?
+    // 2. ViewHolder constructor parameter: Setting the layout to use
     // Configure the binding variable for the row_layout.xml file
     inner class ViewHolder(val binding: RowLayoutBinding) : RecyclerView.ViewHolder (binding.root) {
     }
@@ -20,19 +19,19 @@ class MyAdapter(val myItems:MutableList<String>, val clickInterface:ClickDetecto
         return ViewHolder(binding)
     }
 
-    // 3. How many items are in the list of data?
+    // 3. Getting the number of items in the list
     override fun getItemCount(): Int {
         return myItems.size
     }
 
-    // 4. For each row, what data should go in the corresponding layout’s UI elements?
+    // 4. Getting the data that should go in the corresponding layout’s UI elements for each row
     // The “holder” parameter provides access to the row layout’s binding variable
     // The “position” parameter indicates which item in the data source we creating a row for
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.d("", "Creating UI for row #$position")
         // 4a. Get the current item from the list of data
         // This is a String because the data source is a list of Strings
-        // If the data source was a list of Student objects, then use currData:Student
+        // For example: If the data source was a list of Student objects, then use currData:Student
 
         val icon = if (position <= 2) "ic_android_black_24" else "baseline_check_24"
         val myImageId:Int = holder.itemView.context.resources.getIdentifier(icon, "drawable", holder.itemView.context.packageName)
